27368d81dab8477f3609815ea639889e12c93b77
emowtet <emwsec@gmail.com>
2025-06-14 02:50:37 -0300

    style: reorder printf

--- test/test_isalpha.c
+++ test/test_isalpha.c
@@ -8,8 +8,8 @@ int main(void)
     printf("ft_isalpha('z') = %d | Correct output: 1\n", ft_isalpha('z'));  
     printf("ft_isalpha('4') = %d | Correct output: 0\n", ft_isalpha('4'));  
     printf("ft_isalpha('@') = %d | Correct output: 0\n", ft_isalpha('@'));  
-	printf("ft_isalpha(65)  = %d | Correct output: 1\n", ft_isalpha(65));  // 'A'  
-	printf("ft_isalpha(99)  = %d | Correct output: 1\n", ft_isalpha(99));  // 'c'   
+    printf("ft_isalpha(65)  = %d | Correct output: 1\n", ft_isalpha(65));  // 'A'  
+    printf("ft_isalpha(99)  = %d | Correct output: 1\n", ft_isalpha(99));  // 'c'   
     printf("ft_isalpha(0)   = %d | Correct output: 0\n", ft_isalpha(0));    
     printf("ft_isalpha(300) = %d | Correct output: 0\n", ft_isalpha(300));  
     printf("ft_isalpha(-10) = %d | Correct output: 0\n", ft_isalpha(-10));  
